e7bb7c78a11dd23571270a6451241ef80fcd7e7d
Konnov Dmitriy <yourgitemail@example.com>
2022-09-05 13:51:48 +0400

    Firist commit

---
+++ create_db.sql
@@ -0,0 +1,48 @@
+
+/* Таблица "Треки" */
+create table traks.track(
+	track_id serial primary key, /* Первичный ключ */
+	name_track varchar(100) not null, /* Наименование трека */
+	duration int not null, /* Продолжительность в минутах */
+	compilation_id int REFERENCES traks.compilation(compilation_id), /* Ключ из таблицы "Сборник" */
+	album_id int REFERENCES traks.album(album_id) /* Ключ из таблицы "Альбом" */
+);
+
+/* Таблица "Сборник" */
+create table traks.compilation(
+	compilation_id serial primary key, /* Первичный ключ */
+	com_release_date date not null /* Дата выхода сборника */
+);
+
+/* Таблица "Альбом" */
+create table traks.album(
+	album_id serial primary key, /* Первичный ключ */
+	name_track varchar(100) not null, /* Наименование альбома */
+	release_date date not null /* Дата выхода альбома */
+);
+
+/* Таблица "Автор" */
+create table traks.author(
+	author_id serial primary key, /* Первичный ключ */
+	name_track varchar(100) not null /* Наименование авторов */
+);
+
+/* Таблица связи авторов и альбомов */
+create table traks.alb_auth(
+	alb_auth_is serial primary key, /* Первичный ключ */
+	author_id INTEGER NOT NULL REFERENCES traks.author(author_id), /* Идентификатор авторов */
+	album_id INTEGER NOT NULL REFERENCES traks.album(album_id) /* Идентификатор альбомов */
+);
+
+/* Таблица "Жанр" */
+create table traks.genre(
+	genre_id serial primary key, /* Первичный ключ */
+	genre_name varchar(100) not null /* Наименование жанров */
+);
+
+/* Таблица связи авторов и жанров */
+create table traks.auth_genr(
+	auth_genr_id serial primary key, /* Первичный ключ */
+	author_id INTEGER NOT NULL REFERENCES traks.author(author_id), /* Идентификатор авторов */
+	genre_id INTEGER NOT NULL REFERENCES traks.genre(genre_id) /* Идентификатор альбомов */
+);
---
+++ drop.sql
@@ -0,0 +1,12 @@
+--дропнуть схему
+drop schema traks.album;
+
+--дропнуть таблицу
+drop table traks.album;
+drop table traks.track;
+drop table traks.compilation;
+drop table traks.author;
+drop table traks.alb_auth;
+drop table traks.genre;
+drop table traks.auth_genr;
+
