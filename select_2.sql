d1cf7f393b616f353426200497cc9372b7d5aff5
Konnov Dmitriy <yourgitemail@example.com>
2022-09-27 01:30:49 +0400

    Ошибки select_2

--- select_2.sql
+++ select_2.sql
@@ -1,4 +1,4 @@
--- количество исполнителей в каждом жанре;
+--(1) количество исполнителей в каждом жанре;
 
 select g.genre_name, count(g.genre_name) as "Кол-во исполнителей"
 from traks.auth_genr ag 
@@ -9,7 +9,7 @@ from traks.auth_genr ag
 group by g.genre_name
 order by count(g.genre_name) asc
 	
--- количество треков, вошедших в альбомы 2019-2020 годов;
+--(2) количество треков, вошедших в альбомы 2019-2020 годов;
 /* В скрипт INSERT добавлена запись об альбоме входящем в указанный диапозон */
 
 select a.name_album, count(a.name_album) as "Кол-во треков" 
@@ -17,12 +17,12 @@ from traks.track t join traks.album a on t.album_id = a.album_id
 where a.release_date between '2019-01-01' and '2020-12-31'
 group by a.name_album;
 
--- средняя продолжительность треков по каждому альбому;
+--(3) средняя продолжительность треков по каждому альбому;
 
 select cast(avg(t.duration) as NUMERIC(6,2))  as "Ср. кол-во мин."
 from traks.track t join traks.album a on t.album_id = a.album_id 
 
--- все исполнители, которые не выпустили альбомы в 2020 году;
+--(4) все исполнители, которые не выпустили альбомы в 2020 году;
 
 select a.name_author 
 from traks.author a 
@@ -37,91 +37,70 @@ where a.author_id not in (
 	)
 
 
--- названия сборников, в которых присутствует конкретный исполнитель (выберите сами);
+--(5) названия сборников, в которых присутствует конкретный исполнитель (выберите сами);
 	
-with comp_auth as(
-	select *
-	from (
-			select c.name_compilation, t.album_id 
-			from traks.track_comp tc 
-				join traks.track t 
-					on t.track_id = tc.track_id 
-				join traks.compilation c 
-					on c.compilation_id = tc.compilation_id) as t1
-		join (
-			select a2.name_author, a.album_id 
-			from traks.alb_auth aa 
-				join traks.album a 
-					on aa.album_id = a.album_id 
-				join traks.author a2 
-					on aa.author_id = a2.author_id ) as t2
-		on t1.album_id = t2.album_id
-)
-select ca.name_compilation
-from comp_auth ca
-where ca.name_author = 'Michael Jackson';
-		
--- название альбомов, в которых присутствуют исполнители более 1 жанра;
+select c.name_compilation 
+from traks.alb_auth aa 
+	join traks.author a 
+		on aa.author_id = a.author_id 
+	join traks.album a2 
+		on aa.album_id = a2.album_id
+	join traks.track t 
+		on a2.album_id = t.album_id 
+	left join traks.track_comp tc 
+		on t.track_id = tc.track_id 
+	left join traks.compilation c 
+		on tc.compilation_id = c.compilation_id
+where a.name_author = 'Michael Jackson';
+
+
+--(6) название альбомов, в которых присутствуют исполнители более 1 жанра;
 /* В скрипт INSERT добавлена запись, сделав панк группу еще и рэперами */
 
-select name_album
-from traks.alb_auth aa 
+select a.name_album
+from traks.alb_auth aa
 	join traks.album a 
-		on aa.album_id = a.album_id 
+		on aa.album_id = a.album_id
 	join traks.author a2 
-		on aa.author_id = a2.author_id
-where a2.author_id in (
-	select a.author_id
+		on aa.author_id = a2.author_id 
+where a2.author_id = (select author_id
 	from traks.auth_genr ag 
-		join traks.author a 
-			on ag.author_id = a.author_id 
-		join traks.genre g 
-			on ag.genre_id = g.genre_id 
-	group by a.author_id
-	having count(a.author_id) > 1
-);
-
--- наименование треков, которые не входят в сборники;
+	group by ag.author_id 
+	having count(ag.author_id) > 1)
+	
+--(7) наименование треков, которые не входят в сборники;
 
 select t.name_track 
 from traks.track t 
-	left join(
-		select t.track_id, tc.compilation_id  
-		from traks.track_comp tc 
-			join traks.track t 
-				on t.track_id = tc.track_id 
-			join traks.compilation c 
-				on c.compilation_id = tc.compilation_id
-		) t2
-	on t.track_id = t2.track_id
-where t2.track_id is null;
+	left join traks.track_comp tc 
+		on t.track_id  = tc.track_id 
+where tc.compilation_id is null
 	
--- исполнителя(-ей), написавшего самый короткий по продолжительности трек (теоретически таких треков может быть несколько);
+--(8) исполнителя(-ей), написавшего самый короткий по продолжительности трек (теоретически таких треков может быть несколько);
 		
-select t1.name_author
-from (
-		select a.album_id, a2.name_author 
-		from traks.alb_auth aa 
-			join traks.album a 
-				on aa.album_id = a.album_id 
-			join traks.author a2 
-				on aa.author_id = a2.author_id
-		) t1
-	join traks.track t 
-		on t1.album_id = t.album_id 
+select a2.name_author  
+from traks.track t 
+	join traks.album a 
+		on t.album_id = a.album_id
+	join traks.alb_auth aa 
+		on aa.album_id = a.album_id 
+	join traks.author a2 
+		on a2.author_id = aa.author_id 
 where t.duration = (select min(duration)
 	from traks.track);
 
--- название альбомов, содержащих наименьшее количество треков;
+--(9) название альбомов, содержащих наименьшее количество треков;
 
-with t1 as(
-	select a.name_album, count(a.name_album) cna
-	from traks.track t 
-		join traks.album a 
-			on t.album_id  = a.album_id 
-	group by a.name_album
+with t as
+(
+select a.name_album, count(a.name_album) cna
+from traks.track t 
+	join traks.album a 
+		on t.album_id  = a.album_id 
+group by a.name_album 
 )
-select t1.name_album
-from t1
-where cna in (select min(cna)
-	from t1)
+
+select name_album
+from t
+where cna = (select min(cna)
+	from t)
